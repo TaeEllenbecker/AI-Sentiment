8e2a7c0edd84c5e273544322a137eaf0a0821dfb
Tae Ellenbecker <ellenbeckertae@gmail.com>
2024-10-15 18:41:12 -0500

    Updated new hugging face model in imagetotext.py, requirements.txt updated for the new model, .gitignore updated

--- imagetotext.py
+++ imagetotext.py
@@ -1,48 +1,61 @@
-import os
+from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
+from qwen_vl_utils import process_vision_info
+import torch
+torch.cuda.empty_cache()
+# default: Load the model on the available device(s)
+model = Qwen2VLForConditionalGeneration.from_pretrained(
+    "Qwen/Qwen2-VL-7B-Instruct", torch_dtype="auto", device_map="auto"
+)
 
-from huggingface_hub import hf_hub_download
+# We recommend enabling flash_attention_2 for better acceleration and memory saving, especially in multi-image and video scenarios.
+# model = Qwen2VLForConditionalGeneration.from_pretrained(
+#     "Qwen/Qwen2-VL-7B-Instruct",
+#     torch_dtype=torch.bfloat16,
+#     attn_implementation="flash_attention_2",
+#     device_map="auto",
+# )
 
-HUGGING_FACE_API_KEY = os.environ.get("HUGGING_FACE_API_KEY")
+# default processer
+processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-7B-Instruct")
 
-model_id = "stepfun-ai/GOT-OCR2_0"
+# The default range for the number of visual tokens per image in the model is 4-16384. You can set min_pixels and max_pixels according to your needs, such as a token count range of 256-1280, to balance speed and memory usage.
+# min_pixels = 256*28*28
+# max_pixels = 1280*28*28
+# processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-7B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
 
-filenames = ["config.json","generation_config.json","got_vision_b.py","model.safetensors","modeling_GOT.py","qwen.tiktoken",
-"render_tools.py","special_tokens_map.json","tokenization_qwen.py","tokenizer_config.json"
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "image",
+                "image": "product-manager-business-card.jpg",
+            },
+            {"type": "text", "text": "Output only the text on the screen."},
+        ],
+    }
 ]
 
-for filename in filenames:
-    downloaded_model_path = hf_hub_download(repo_id = model_id, filename = filename, token = HUGGING_FACE_API_KEY)
+# Preparation for inference
+text = processor.apply_chat_template(
+    messages, tokenize=False, add_generation_prompt=True
+)
+image_inputs, video_inputs = process_vision_info(messages)
+inputs = processor(
+    text=[text],
+    images=image_inputs,
+    videos=video_inputs,
+    padding=True,
+    return_tensors="pt",
+)
+inputs = inputs.to("cuda")
 
-print(downloaded_model_path)
-
-
-from transformers import AutoModel, AutoTokenizer
-
-tokenizer = AutoTokenizer.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True)
-model = AutoModel.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True, low_cpu_mem_usage=True, device_map='cuda', use_safetensors=True, pad_token_id=tokenizer.eos_token_id)
-model = model.eval().cuda()
-
-
-# input your test image
-image_file = 'product-manager-business-card.jpg'
-
-# plain texts OCR
-res = model.chat(tokenizer, image_file, ocr_type='ocr')
-
-# format texts OCR:
-# res = model.chat(tokenizer, image_file, ocr_type='format')
-
-# fine-grained OCR:
-# res = model.chat(tokenizer, image_file, ocr_type='ocr', ocr_box='')
-# res = model.chat(tokenizer, image_file, ocr_type='format', ocr_box='')
-# res = model.chat(tokenizer, image_file, ocr_type='ocr', ocr_color='')
-# res = model.chat(tokenizer, image_file, ocr_type='format', ocr_color='')
-
-# multi-crop OCR:
-# res = model.chat_crop(tokenizer, image_file, ocr_type='ocr')
-# res = model.chat_crop(tokenizer, image_file, ocr_type='format')
-
-# render the formatted OCR results:
-# res = model.chat(tokenizer, image_file, ocr_type='format', render=True, save_render_file = './demo.html')
-
-print(res)
+# Inference: Generation of the output
+generated_ids = model.generate(**inputs, max_new_tokens=128)
+generated_ids_trimmed = [
+    out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+]
+output_text = processor.batch_decode(
+    generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+)
+print(output_text)
